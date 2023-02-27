52e96c7dd018bc5cda8c741399f6e1b3b3057bfe
sw965 <none>
2023-02-27 14:48:08 +0900

    モンテカルロ木探索の機能の追加

---
+++ mcts/mcts.go
@@ -0,0 +1,154 @@
+package mcts
+
+import (
+	"fmt"
+	"math/rand"
+	"github.com/sw965/omw"
+	"github.com/sw965/crow/pucb"
+)
+
+type Policy[S any, A comparable] func(*S) map[A]float64
+type LeafEval[S any] func(*S) float64
+type BackwardEval[S any, A comparable] func(float64, *Node[S, A]) float64
+
+type Eval[S any, A comparable]  struct {
+	Leaf LeafEval[S]
+	Backward BackwardEval[S, A]
+}
+
+type StatePush[S any, A comparable] func(S, A) (S, error)
+type IsEndState[S any] func(*S) bool
+
+type StateFunc[S any, A comparable] struct {
+	Push StatePush[S, A]
+	IsEnd IsEndState[S]
+}
+
+type NodeEqual[S any, A comparable] func(*Node[S, A], *Node[S, A]) bool
+type NewNode [S any, A comparable] func(*S, Policy[S, A]) (*Node[S, A], error)
+
+type NodeFunc[S any, A comparable] struct {
+	Equal NodeEqual[S, A]
+	New NewNode[S, A]
+}
+
+type Func[S any, A comparable] struct {
+	State StateFunc[S, A]
+	Node NodeFunc[S, A]
+}
+
+type Node[S any, A comparable] struct {
+	State S
+	LegalActions []A
+	PUCBByAction pucb.ManagerByKey[A]
+	NextNodes Nodes[S, A]
+	SelectCount     int
+	ID int
+}
+
+type Nodes[S any, A comparable] []*Node[S, A]
+
+func (nodes Nodes[S, A]) Find(node *Node[S, A], equal func(*Node[S, A], *Node[S, A]) bool) (*Node[S, A], error) {
+	for _, v := range nodes {
+		if equal(node, v) {
+			return v, nil
+		}
+	}
+	return &Node[S, A]{}, fmt.Errorf("一致するNodeが見つからなかった")
+}
+
+type Select[S any, A comparable] struct {
+	Node *Node[S, A]
+	Action A
+}
+
+type Selects[S any, A comparable] []Select[S, A]
+
+func (ss Selects[S, A]) Backward(leafEvalY float64, eval BackwardEval[S, A]) {
+	for _, s := range ss {
+		node := s.Node
+		action := s.Action
+		node.PUCBByAction[action].AccumReward += eval(leafEvalY, node)
+		node.PUCBByAction[action].Trial += 1
+		node.SelectCount = 0
+	}
+}
+
+func (node *Node[S, A])SelectAndExpansion(allNodes Nodes[S, A], policy Policy[S, A], X float64, r *rand.Rand, f *Func[S, A], capSize int) (S, Nodes[S, A], Selects[S, A], error) {
+	selects := make(Selects[S, A], 0, capSize)
+	state := node.State
+
+	for {
+		actions := node.PUCBByAction.MaxKeys(X)
+		action := omw.RandomChoice(actions, r)
+		selects = append(selects, Select[S, A]{Node:node, Action:action})
+		node.SelectCount += 1
+
+		state, err := f.State.Push(state, action)
+
+		if err != nil {
+			return state, Nodes[S, A]{}, Selects[S, A]{}, err
+		}
+
+		if f.State.IsEnd(&state) {
+			break
+		}
+
+		//nextNodesの中に、同じstateが存在するならば、それを次のNodeとする
+		//nextNodesの中に、同じstateが存在しないなら、allNodesの中から同じstateが存在しないかを調べる。
+		//allNodesの中に、同じstateが存在するならば、次回から高速に探索出来るように、nextNodesに追加して、次のnodeとする。
+		//nextNodesにもallNodesにも同じstateが存在しないなら、新しくnodeを作り、
+		//nextNodesと、allNodesに追加し、新しく作ったnodeを次のnodeとし、select処理を終了する。
+
+		nextNode, err := node.NextNodes.Find(node, f.Node.Equal)
+		if err != nil {
+			nextNode, err = allNodes.Find(node, f.Node.Equal)
+			if err == nil {
+				node.NextNodes = append(node.NextNodes, nextNode)
+			} else {
+				nextNode, err = f.Node.New(&state, policy)
+				if err != nil {
+					return state, Nodes[S, A]{}, Selects[S, A]{}, err
+				}
+				allNodes = append(allNodes, nextNode)
+				node.NextNodes = append(node.NextNodes, nextNode)
+				break
+			}
+		}
+
+		if nextNode.SelectCount == 1 {
+			break
+		}
+		node = nextNode
+	}
+	return state, allNodes, selects, nil
+}
+
+func Run[S any, A comparable](simulation int, rootState S, policy Policy[S, A], eval Eval[S, A], X float64, r *rand.Rand, f *Func[S, A]) (Nodes[S, A], error) {
+	rootNode, err := f.Node.New(&rootState, policy)
+	if err != nil {
+		return Nodes[S, A]{}, err
+	}
+	allNodes := Nodes[S, A]{rootNode}
+	node := rootNode
+	capSize := 0
+	var leafState S
+	var selects Selects[S, A]
+
+	for i := 0; i < simulation; i++ {
+		leafState, allNodes, selects, err = node.SelectAndExpansion(allNodes, policy, X, r, f, capSize + 1)
+		if err != nil {
+			return Nodes[S, A]{}, err
+		}
+		capSize = len(selects)
+
+		leafEvalY := eval.Leaf(&leafState)
+		if err != nil {
+			return Nodes[S, A]{}, err
+		}
+
+		selects.Backward(leafEvalY, eval.Backward)
+		node = rootNode
+	}
+	return allNodes, nil
+}
