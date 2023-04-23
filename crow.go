344f7b1c2db833e195a49473fa03fef002af7a86
sw965 <none>
2023-04-24 00:52:00 +0900

    dpuctのテスト追加

--- crow.go
+++ crow.go
@@ -32,6 +32,14 @@ func PolicyUpperConfidenceBound(c, p, v float64, n, a int) float64 {
 
 type Reward float64
 
+const (
+	WIN_REWARD = Reward(1.0)
+	LOSE_REWARD = Reward(0.0)
+	DRAW_REWARD = Reward(0.5)
+	NIL_REWARD = Reward(-128.0)
+)
+
+
 type AlternatelyMoveGamePlayer[S any, A comparable] func(*S) A
 type SimultaneousMoveGamePlayer[S any, A comparable] func(*S) []A
 
@@ -43,17 +51,6 @@ type StateSimultaneousPushFunc[S any, A comparable] func(S, ...A) S
 
 type EqualStateFunc[S any] func(*S, *S) bool
 type IsEndStateWithRewardFunc[S any] func(*S) (bool, Reward)
-
-func playout[S any, A comparable](state S, pushMethod func(S) S, isEndWithReward func(*S) (bool, Reward)) Reward {
-	for {
-		isEnd, reward := isEndWithReward(&state)
-		if isEnd {
-			return reward
-		}
-		state = pushMethod(state)
-	}
-	return -128.0
-}
 
 type AlternatelyMoveGameFunCaller[S any, A comparable] struct {
 	Player AlternatelyMoveGamePlayer[S, A]
@@ -69,36 +66,52 @@ func (f *AlternatelyMoveGameFunCaller[S, A]) SetRandomActionPlayer(r *rand.Rand)
 	}
 }
 
-func (f *AlternatelyMoveGameFunCaller[S, A]) PushMethod(state S) S {
-	action := f.Player(&state)
-	return f.Push(state, action)
-}
-
-func (f *AlternatelyMoveGameFunCaller[S, A]) PlayoutMethod(state S) Reward {
-	return playout[S, A](state, f.PushMethod, f.IsEndWithReward)
+func (f *AlternatelyMoveGameFunCaller[S, A]) Playout(state S) Reward {
+	for {
+		isEnd, reward := f.IsEndWithReward(&state)
+		if isEnd {
+			return reward
+		}
+		action := f.Player(&state)
+		state = f.Push(state, action)
+	}
+	return NIL_REWARD
 }
 
 type SimultaneousMoveGameFunCaller[S any, A comparable] struct {
 	Player SimultaneousMoveGamePlayer[S, A]
-	LegalActions SimultaneousMoveGameLegalActionssFunc[S, A]
+	LegalActionss SimultaneousMoveGameLegalActionssFunc[S, A]
 	Push StateSimultaneousPushFunc[S, A]
 	EqualState EqualStateFunc[S]
 	IsEndWithReward IsEndStateWithRewardFunc[S]
 }
 
-func (f *SimultaneousMoveGameFunCaller[S, A]) PushMethod(state S) S {
-	actions := f.Player(&state)
-	return f.Push(state, actions...)
+func (f *SimultaneousMoveGameFunCaller[S, A]) SetRandomActionPlayer(r *rand.Rand) {
+	f.Player = func(state *S) []A {
+		actionss := f.LegalActionss(state)
+		y := make([]A, len(actionss))
+		for playerI, actions := range actionss {
+			y[playerI] = omw.RandChoice(actions, r)
+		}
+		return y
+	}
 }
 
-func (f *SimultaneousMoveGameFunCaller[S, A]) PlayoutMethod(state S) Reward {
-	return playout[S, A](state, f.PushMethod, f.IsEndWithReward)
+func (f *SimultaneousMoveGameFunCaller[S, A]) Playout(state S) Reward {
+	for {
+		isEnd, reward := f.IsEndWithReward(&state)
+		if isEnd {
+			return reward
+		}
+		actions := f.Player(&state)
+		state = f.Push(state, actions...)
+	}
+	return -128.0
 }
 
 type UtilPUCB struct {
 	AccumReward Reward
 	Trial       int
-	C float64
 	P float64
 }
 
@@ -106,9 +119,9 @@ func (p *UtilPUCB) AverageReward() float64 {
 	return float64(p.AccumReward) / float64(p.Trial+1)
 }
 
-func (p *UtilPUCB) Get(totalTrial int) float64 {
+func (p *UtilPUCB) Get(totalTrial int, c float64) float64 {
 	v := p.AverageReward()
-	return PolicyUpperConfidenceBound(p.C, p.P, v, totalTrial, p.Trial)
+	return PolicyUpperConfidenceBound(c, p.P, v, totalTrial, p.Trial)
 }
 
 type PUCBMapManager[K comparable] map[K]*UtilPUCB
@@ -120,21 +133,21 @@ func (m PUCBMapManager[K]) Trials() []int {
 	}
 	return y
 }
-func (m PUCBMapManager[K]) Max() float64 {
+func (m PUCBMapManager[K]) Max(c float64) float64 {
 	total := omw.Sum(m.Trials()...)
 	y := make([]float64, 0, len(m))
 	for _, v := range m {
-		y = append(y, v.Get(total))
+		y = append(y, v.Get(total, c))
 	}
 	return omw.Max(y...)
 }
 
-func (m PUCBMapManager[K]) MaxKeys() []K {
-	max := m.Max()
+func (m PUCBMapManager[K]) MaxKeys(c float64) []K {
+	max := m.Max(c)
 	total := omw.Sum(m.Trials()...)
 	ks := make([]K, 0, len(m))
 	for k, v := range m {
-		if v.Get(total) == max {
+		if v.Get(total, c) == max {
 			ks = append(ks, k)
 		}
 	}
@@ -169,12 +182,6 @@ type ActionPolicyFunc[S any, A comparable] func(*S) ActionPolicY[A]
 type PUCT_LeafEvalY Reward
 type PUCT_LeafEvalFunc[S any] func(*S) PUCT_LeafEvalY
 
-func NewPUCTPlayoutLeafEvalFunc[S any, A comparable](f *AlternatelyMoveGameFunCaller[S, A]) PUCT_LeafEvalFunc[S] {
-	return func(state *S) PUCT_LeafEvalY {
-		return PUCT_LeafEvalY(f.PlayoutMethod(*state))
-	}
-}
-
 type PUCT_BackwardEvalY float64
 type PUCT_BackwardEvalFunc[S any] func(PUCT_LeafEvalY, *S) PUCT_BackwardEvalY
 
@@ -189,11 +196,11 @@ type PUCT_FunCaller[S any, A comparable] struct {
 	Eval PUCT_EvalFunCaller[S]
 }
 
-func (f *PUCT_FunCaller[S, A]) NewNodeMethod(state *S, c float64) *PUCT_Node[S, A] {
+func (f *PUCT_FunCaller[S, A]) NewNode(state *S) *PUCT_Node[S, A] {
 	py := f.Policy(state)
 	m := PUCBMapManager[A]{}
 	for a, p := range py {
-		m[a] = &UtilPUCB{C:c, P:p}
+		m[a] = &UtilPUCB{P:p}
 	}
 	return &PUCT_Node[S, A]{State:*state, PUCBManager:m}
 }
@@ -215,7 +222,7 @@ func (f *PUCT_FunCaller[S, A]) SetNoPolicy() {
 func (f *PUCT_FunCaller[S, A]) SetPlayoutLeafEval() {
 	var leaf PUCT_LeafEvalFunc[S]
 	leaf = func(state *S) PUCT_LeafEvalY {
-		return PUCT_LeafEvalY(f.Game.PlayoutMethod(*state))
+		return PUCT_LeafEvalY(f.Game.Playout(*state))
 	}
 	f.Eval.Leaf = leaf
 }
@@ -225,6 +232,34 @@ type PUCT_Node[S any, A comparable] struct {
 	PUCBManager PUCBMapManager[A]
 	NextNodes PUCT_Nodes[S, A]
 	SelectCount     int
+}
+
+func (node *PUCT_Node[S, A]) Trial() int {
+	return omw.Sum(node.PUCBManager.Trials()...)
+}
+
+func (node *PUCT_Node[S, A]) ActionPrediction(c float64, r *rand.Rand, cap_ int) []A {
+	y := make([]A, 0, cap_)
+	for {
+		if len(node.NextNodes) == 0 {
+			break
+		}
+
+		action := omw.RandChoice(node.PUCBManager.MaxKeys(c), r)
+		y = append(y, action)
+		max := node.NextNodes[0].Trial()
+		nextNode := node.NextNodes[0]
+
+		for _, nn := range node.NextNodes[1:] {
+			trial := nn.Trial()
+			if trial > max {
+				max = trial
+				nextNode = nn
+			}
+		}
+		node = nextNode
+	}
+	return y
 }
 
 type PUCT_Nodes[S any, A comparable] []*PUCT_Node[S, A]
@@ -269,7 +304,7 @@ func (s *PUCT_Selector[S, A]) SelectAndExpansion(node *PUCT_Node[S, A], allNodes
 		// for a, pucb := range node.PUCBManager {
 		// 	fmt.Println(a, pucb.Get(omw.Sum(node.PUCBManager.Trials()...), len(node.PUCBManager)))
 		// }
-		action := omw.RandChoice(node.PUCBManager.MaxKeys(), r)
+		action := omw.RandChoice(node.PUCBManager.MaxKeys(c), r)
 		selects = append(selects, PUCT_Select[S, A]{Node:node, Action:action})
 
 		node.SelectCount += 1
@@ -293,7 +328,7 @@ func (s *PUCT_Selector[S, A]) SelectAndExpansion(node *PUCT_Node[S, A], allNodes
 			if ok {
 				node.NextNodes = append(node.NextNodes, nextNode)
 			} else {
-				nextNode = f.NewNodeMethod(stateP, c)
+				nextNode = f.NewNode(stateP)
 				allNodes = append(allNodes, nextNode)
 				node.NextNodes = append(node.NextNodes, nextNode)
 				//新しくノードを作成したら、処理を終了する
@@ -317,7 +352,7 @@ type PUCT[S any, A comparable] struct {
 
 func (p *PUCT[S, A]) Run(simulation int, rootState S, c float64, r *rand.Rand) PUCT_Nodes[S, A] {
 	f := p.FunCaller
-	rootNode := f.NewNodeMethod(&rootState, c)
+	rootNode := f.NewNode(&rootState)
 	allNodes := PUCT_Nodes[S, A]{rootNode}
 
 	var leafState S
@@ -345,17 +380,34 @@ type DPUCT_FunCaller[S any, A comparable] struct {
 	LeafEvals DPUCT_LeafEvalsFunc[S]
 }
 
-func (f *DPUCT_FunCaller[S, A]) NewNodeMethod(state *S) *DPUCT_Node[S, A] {
+func (f *DPUCT_FunCaller[S, A]) NewNode(state *S) *DPUCT_Node[S, A] {
 	pys := f.Policies(state)
 	ms := make(PUCBMapManagers[A], len(pys))
 
 	for playerI, py := range pys {
-		ms[playerI] = PUCBMapManager[A]{}
+		m := PUCBMapManager[A]{}
 		for a, p := range py {
-			ms[playerI][a].P = p
+			m[a] = &UtilPUCB{P:p}
 		}
+		ms[playerI] = m
 	}
 	return &DPUCT_Node[S, A]{State:*state, PUCBManagers:ms}
+}
+
+func (f *DPUCT_FunCaller[S, A]) SetNoPolicies() {
+	f.Policies = func(state *S) ActionPolicYs[A] {
+		legalActionss := f.Game.LegalActionss(state)
+		ys := make(ActionPolicYs[A], len(legalActionss))
+		for playerI, actions := range legalActionss {
+			y := map[A]float64{}
+			p := 1.0 / float64(len(actions))
+			for _, action := range actions {
+				y[action] = p
+			}
+			ys[playerI] = y
+		}
+		return ys
+	}
 }
 
 type DPUCT_Node[S any, A comparable] struct {
@@ -363,6 +415,48 @@ type DPUCT_Node[S any, A comparable] struct {
 	PUCBManagers PUCBMapManagers[A]
 	NextNodes DPUCT_Nodes[S, A]
 	SelectCount     int
+}
+
+func (node *DPUCT_Node[S, A]) Trial() int {
+	maxes := make([]int, len(node.PUCBManagers))
+	for playerI, m := range node.PUCBManagers {
+		maxes[playerI] = omw.Max(m.Trials()...)
+	}
+	return omw.Max(maxes...)
+}
+
+func (node *DPUCT_Node[S, A]) ActionPrediction(c float64, r *rand.Rand, cap_ int) [][]A {
+	y := make([][]A, 0, cap_)
+	for {
+
+		if len(node.PUCBManagers) == 0 {
+			break
+		}
+
+		actions := make([]A, len(node.PUCBManagers))
+		for playerI, m := range node.PUCBManagers {
+			actions[playerI] = omw.RandChoice(m.MaxKeys(c), r)
+		}
+		y = append(y, actions)
+
+		if len(node.NextNodes) == 0 {
+			break
+		}
+
+
+		max := node.NextNodes[0].Trial()
+		nextNode := node.NextNodes[0]
+
+		for _, nn := range node.NextNodes[1:] {
+			trial := nn.Trial()
+			if trial > max {
+				max = trial
+				nextNode = nn
+			}
+		}
+		node = nextNode
+	}
+	return y
 }
 
 type DPUCT_Nodes[S any, A comparable] []*DPUCT_Node[S, A]
@@ -408,7 +502,7 @@ func (s *DPUCT_Selector[S, A])SelectAndExpansion(simultaneous int, node *DPUCT_N
 	for {
 		actions := make([]A, simultaneous)
 		for playerI, m := range node.PUCBManagers {
-			actions[playerI] = omw.RandChoice(m.MaxKeys(), r)
+			actions[playerI] = omw.RandChoice(m.MaxKeys(c), r)
 		}
 
 		selects = append(selects, DPUCT_Select[S, A]{Node:node, Actions:actions})
@@ -433,7 +527,7 @@ func (s *DPUCT_Selector[S, A])SelectAndExpansion(simultaneous int, node *DPUCT_N
 			if ok {
 				node.NextNodes = append(node.NextNodes, nextNode)
 			} else {
-				nextNode = f.NewNodeMethod(stateP)
+				nextNode = f.NewNode(stateP)
 				allNodes = append(allNodes, nextNode)
 				node.NextNodes = append(node.NextNodes, nextNode)
 				//新しくノードを作成したら、処理を終了する
@@ -454,8 +548,8 @@ type DPUCT[S any, A comparable] struct {
 	FunCaller DPUCT_FunCaller[S, A]
 }
 
-func (d *DPUCT[S, A]) Run(simulation int, rootState S, c float64, r *rand.Rand) (DPUCT_Nodes[S, A], error) {
-	rootNode := d.FunCaller.NewNodeMethod(&rootState)
+func (d *DPUCT[S, A]) Run(simulation int, rootState S, c float64, r *rand.Rand) DPUCT_Nodes[S, A] {
+	rootNode := d.FunCaller.NewNode(&rootState)
 	allNodes := DPUCT_Nodes[S, A]{rootNode}
 	simultaneous := len(rootNode.PUCBManagers)
 
@@ -468,5 +562,5 @@ func (d *DPUCT[S, A]) Run(simulation int, rootState S, c float64, r *rand.Rand) 
 		ys := d.FunCaller.LeafEvals(&leafState)
 		selects.Backward(ys)
 	}
-	return allNodes, nil
+	return allNodes
 }
--- crow_test.go
+++ crow_test.go
@@ -9,11 +9,13 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-func Test(t *testing.T) {
+func TestPUCT(t *testing.T) {
 	r := omw.NewMt19937()
+	ALL_COINS := omw.MakeIntegerRange[[]int](0, 5, 1)
+	MAX_COIN := omw.Max(ALL_COINS...)
 
 	legalActions := func(coins *[]int) []int {
-		return omw.MakeIntegerRange[[]int](0, 10, 1)
+		return ALL_COINS
 	}
 
 	push := func(coins []int, coin int) []int {
@@ -31,7 +33,8 @@ func Test(t *testing.T) {
 
 	isEndWithReward := func(coins *[]int) (bool, crow.Reward) {
 		reward := crow.Reward(omw.Sum(*coins...))
-		return len(*coins) == 5, reward / 45.0
+		n := 10
+		return len(*coins) == n, reward / crow.Reward(MAX_COIN * n)
 	}
 
 	game := crow.AlternatelyMoveGameFunCaller[[]int, int]{
@@ -54,7 +57,76 @@ func Test(t *testing.T) {
 	puct := crow.PUCT[[]int, int]{FunCaller:fnCaller}
 	init := []int{}
 	allNodes := puct.Run(19600, init, math.Sqrt(25.0), r)
-	for a, pucb := range allNodes[0].PUCBManager {
-		fmt.Println(a, pucb.AccumReward, pucb.Trial, pucb.AverageReward())
+	for _, coin := range ALL_COINS {
+		pucb := allNodes[0].PUCBManager[coin]
+		fmt.Println(coin, pucb.AccumReward, pucb.Trial, pucb.AverageReward())
 	}
+
+	fmt.Println(allNodes[0].ActionPrediction(math.Sqrt(2), r, 64))
+}
+
+type PPSState struct {
+	Hand1 string
+	Hand2 string
+}
+
+func TestDPUCT(t *testing.T) {
+	r := omw.NewMt19937()
+	var HANDS = []string{"グー", "チョキ", "パー"}
+
+	legalActionss := func(state *PPSState) ([][]string) {
+		return [][]string{HANDS, HANDS}
+	}
+
+	push := func(state PPSState, hands ...string) PPSState {
+		return PPSState{Hand1:hands[0], Hand2:hands[1]}
+	}
+
+	equal := func(state1, state2 *PPSState) bool {
+		return *state1 == *state2
+	}
+
+	isEndWithReward := func(state *PPSState) (bool, crow.Reward) {
+		rewards := map[string]map[string]crow.Reward{
+			"グー":map[string]crow.Reward{"チョキ":1.0, "パー":0.0},
+			"チョキ":map[string]crow.Reward{"グー":0.0, "パー":1.0},
+			"パー":map[string]crow.Reward{"グー":1.0, "チョキ":0.0},
+		}
+		if state.Hand1 == state.Hand2 {
+			return true, 0.5
+		} else {
+			return true, rewards[state.Hand1][state.Hand2]
+		}
+	}
+
+	game := crow.SimultaneousMoveGameFunCaller[PPSState, string]{
+		LegalActionss:legalActionss,
+		Push:push,
+		EqualState:equal,
+		IsEndWithReward:isEndWithReward,
+	}
+
+	game.SetRandomActionPlayer(r)
+
+	leafEvals := func(state *PPSState) crow.DPUCT_LeafEvalYs {
+		_, reward := isEndWithReward(state)
+		leaf := crow.DPUCT_LeafEvalY(reward)
+		return crow.DPUCT_LeafEvalYs{leaf, 1.0 - leaf}
+	}
+
+	fnCaller := crow.DPUCT_FunCaller[PPSState, string]{
+		Game:game,
+		LeafEvals:leafEvals,
+	}
+	fnCaller.SetNoPolicies()
+
+	dpuct := crow.DPUCT[PPSState, string]{FunCaller:fnCaller}
+	allNodes := dpuct.Run(1960, PPSState{}, math.Sqrt(2), r)
+	for i, m := range allNodes[0].PUCBManagers {
+		for a, pucb := range m {
+			fmt.Println(i, a, pucb.AverageReward(), pucb.Trial)
+		}
+	}
+
+	fmt.Println(allNodes[0].ActionPrediction(math.Sqrt(2), r, 64))
 }
