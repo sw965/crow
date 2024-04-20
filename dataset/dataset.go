1635dace9e55cff25325cd91a6dd9de88f43fb23
sw965 <none>
2024-04-20 16:57:16 +0900

    mnist周りの命名の変更

--- dataset/dataset.go
+++ dataset/dataset.go
@@ -5,31 +5,31 @@ import (
 	"github.com/sw965/crow/tensor"
 )
 
-var MNIST_PATH = omw.SW965_PATH + "crow/mnist_json/"
+var FLAT_MNIST_PATH = omw.SW965_PATH + "crow/flat_mnist_json/"
 
-type Mnist struct {
+type FlatMnist struct {
 	TrainImg tensor.D2
 	TrainLabel tensor.D2
 	TestImg tensor.D2
 	TestLabel tensor.D2
 }
 
-func LoadMnist() (Mnist, error) {
-	trainImg, err := omw.LoadJSON[tensor.D2](MNIST_PATH + "train_img" + omw.JSON_EXTENSION)
+func LoadFlatMnist() (FlatMnist, error) {
+	trainImg, err := omw.LoadJSON[tensor.D2](FLAT_MNIST_PATH + "train_img" + omw.JSON_EXTENSION)
 	if err != nil {
 		return Mnist{}, err
 	}
 
-	trainLabel, err := omw.LoadJSON[tensor.D2](MNIST_PATH + "train_onehot_label" + omw.JSON_EXTENSION)
+	trainLabel, err := omw.LoadJSON[tensor.D2](FLAT_MNIST_PATH + "train_label" + omw.JSON_EXTENSION)
 	if err != nil {
 		return Mnist{}, err
 	}
 
-	testImg, err := omw.LoadJSON[tensor.D2](MNIST_PATH + "test_img" + omw.JSON_EXTENSION)
+	testImg, err := omw.LoadJSON[tensor.D2](FLAT_MNIST_PATH + "test_img" + omw.JSON_EXTENSION)
 	if err != nil {
 		return Mnist{}, err
 	}
 
-	testLabel, err := omw.LoadJSON[tensor.D2](MNIST_PATH + "test_onehot_label" + omw.JSON_EXTENSION)
+	testLabel, err := omw.LoadJSON[tensor.D2](FLAT_MNIST_PATH + "test_label" + omw.JSON_EXTENSION)
 	return Mnist{TrainImg:trainImg, TrainLabel:trainLabel, TestImg:testImg, TestLabel:testLabel}, err
 }
