a1e58cc4d5daf7b25ad3c786bb99a93e581694d4
sw965 <none>
2024-05-14 19:33:09 +0900

    機能の追加

--- mlfuncs/mlfuncs.go
+++ mlfuncs/mlfuncs.go
@@ -5,6 +5,44 @@ import (
 	"github.com/sw965/crow/tensor"
 )
 
+func NewMultiHotTensor(bss ...[]bool) (tensor.D1, error) {
+	if len(bss) == 0 {
+		return tensor.D1{}, nil
+	}
+
+	bssN := len(bss)
+	bsNs := make([]int, bssN)
+	retN := 1
+	for i, bs := range bss {
+		n := len(bs)
+		bsNs[i] = n
+		retN *= n
+	}
+	hotIdxs := make([]int, 0, retN)
+
+	var f func(int, int)
+	f = func(bssIdx, hotIdx int) {
+		if bssIdx == bssN {
+			hotIdxs = append(hotIdxs, hotIdx)
+			return
+		}
+
+		bs := bss[bssIdx]
+		for i, b := range bs {
+			if b {
+				f(bssIdx+1, hotIdx*len(bs)+i)
+			}
+		}
+	}
+	f(0, 0)
+
+	ret := make(tensor.D1, retN)
+	for _, idx := range hotIdxs {
+		ret[idx] = 1.0
+	}
+	return ret, nil
+}
+
 func Identity[X any](x X) X {
 	return x
 }
