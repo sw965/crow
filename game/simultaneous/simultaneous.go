533b03b355cf0060e0172a36b6df2bcfc279f165
sw965 <none>
2024-06-11 21:07:46 +0900

    学習パラメーターの保存機能の追加

--- game/simultaneous/simultaneous.go
+++ game/simultaneous/simultaneous.go
@@ -62,38 +62,6 @@ func (g *Game[S, ASS, AS, A]) Play(state S, f func(*S, int) bool) (S, error) {
 	return state, nil
 }
 
-func (g *Game[S, ASS, AS, A]) PlayWithHistory(state S, f func(*S, int) bool, c int) (S, []S, ASS, error) {
-	i := 0
-	stateHistory := make([]S, 0, c)
-	jointActionHistory := make(ASS, 0, c)
-	for {
-		if g.IsEnd(&state) || f(&state, i) {
-			break
-		}
-
-		jointAction, err := g.Player(&state)
-		if err != nil {
-			var s S
-			return s, []S{}, ASS{}, err
-		}
-
-		stateHistory = append(stateHistory, state)
-		jointActionHistory = append(jointActionHistory, jointAction)
-
-		state, err = g.Push(state, jointAction)
-		if err != nil {
-			var s S
-			return s, []S{}, ASS{}, err
-		}
-		i += 1
-	}
-	return state, stateHistory, jointActionHistory, nil
-}
-
 func (g *Game[S, ASS, AS, A]) Playout(state S) (S, error) {
 	return g.Play(state, func(_ *S, _ int) bool { return false })
-}
-
-func (g *Game[S, ASS, AS, A]) PlayoutWithHistory(state S, c int) (S, []S, ASS, error) {
-	return g.PlayWithHistory(state, func(_ *S, _ int) bool { return false }, c)
 }
--- model/1d/1d.go
+++ model/1d/1d.go
@@ -11,12 +11,27 @@ import (
 	"github.com/sw965/crow/mlfuncs/2d"
 	"github.com/sw965/crow/mlfuncs/3d"
 	omwslices "github.com/sw965/omw/slices"
+	omwjson "github.com/sw965/omw/json"
 )
 
+type Param struct {
+	D1 tensor.D1
+	D2 tensor.D2
+	D3 tensor.D3
+}
+
+func (p *Param) WriteJSON(path string) error {
+	err := omwjson.Write[Param](p, path)
+	return err
+}
+
+func (p *Param) LoadJSON(path string) (Param, error) {
+	param, err := omwjson.Load[Param](path)
+	return param, err
+}
+
 type Variable struct {
-	Param1D tensor.D1
-	Param2D tensor.D2
-	Param3D tensor.D3
+	Param Param
 
 	grad1D tensor.D1
 	grad2D tensor.D2
@@ -36,9 +51,9 @@ func (v *Variable) GetGrad3D() tensor.D3 {
 }
 
 func (v *Variable) Init() {
-	v.grad1D = tensor.NewD1ZerosLike(v.Param1D)
-	v.grad2D = tensor.NewD2ZerosLike(v.Param2D)
-	v.grad3D = tensor.NewD3ZerosLike(v.Param3D)
+	v.grad1D = tensor.NewD1ZerosLike(v.Param.D1)
+	v.grad2D = tensor.NewD2ZerosLike(v.Param.D2)
+	v.grad3D = tensor.NewD3ZerosLike(v.Param.D3)
 }
 
 func (v *Variable) GradMulScaler(lr float64) {
@@ -48,9 +63,15 @@ func (v *Variable) GradMulScaler(lr float64) {
 }
 
 func (v *Variable) SGD() {
-	v.Param1D.Sub(v.grad1D)
-	v.Param2D.Sub(v.grad2D)
-	v.Param3D.Sub(v.grad3D)
+	v.Param.D1.Sub(v.grad1D)
+	v.Param.D2.Sub(v.grad2D)
+	v.Param.D3.Sub(v.grad3D)
+}
+
+func (v *Variable) SetParam(param Param) {
+	v.Param.D1.Copy(param.D1)
+	v.Param.D2.Copy(param.D2)
+	v.Param.D3.Copy(param.D3)
 }
 
 type Sequential struct {
@@ -85,33 +106,35 @@ func NewSequential(variable Variable) Sequential {
 
 func NewStandardAffine(xn, h1, h2, yn int, c, threshold float64, r *rand.Rand) (Sequential, Variable) {
 	variable := Variable{
-		Param1D:tensor.D1{
-			0.1,
-			0.1,
-		},
+		Param:Param{
+			D1:tensor.D1{
+				0.1,
+				0.1,
+			},
+			
+			D2:tensor.D2{
+				tensor.NewD1Zeros(h1),
+				tensor.NewD1Zeros(h2),
+				tensor.NewD1Zeros(yn),
+			},
 
-		Param2D:tensor.D2{
-			tensor.NewD1Zeros(h1),
-			tensor.NewD1Zeros(h2),
-			tensor.NewD1Zeros(yn),
-		},
-
-		Param3D:tensor.D3{
-			tensor.NewD2He(xn, h1, r),
-			tensor.NewD2He(h1, h2, r),
-			tensor.NewD2He(h2, yn, r),
+			D3:tensor.D3{
+				tensor.NewD2He(xn, h1, r),
+				tensor.NewD2He(h1, h2, r),
+				tensor.NewD2He(h2, yn, r),
+			},
 		},
 	}
 	variable.Init()
 
 	forwards := layer1d.Forwards{
-		layer1d.NewAffineForward(variable.Param3D[0], variable.Param2D[0], variable.GetGrad3D()[0], variable.GetGrad2D()[0]),
-		layer1d.NewParamReLUForward(&variable.Param1D[0], &variable.GetGrad1D()[0]),
+		layer1d.NewAffineForward(variable.Param.D3[0], variable.Param.D2[0], variable.GetGrad3D()[0], variable.GetGrad2D()[0]),
+		layer1d.NewParamReLUForward(&variable.Param.D1[0], &variable.GetGrad1D()[0]),
 
-		layer1d.NewAffineForward(variable.Param3D[1], variable.Param2D[1], variable.GetGrad3D()[1], variable.GetGrad2D()[1]),
-		layer1d.NewParamReLUForward(&variable.Param1D[1], &variable.GetGrad1D()[1]),
+		layer1d.NewAffineForward(variable.Param.D3[1], variable.Param.D2[1], variable.GetGrad3D()[1], variable.GetGrad2D()[1]),
+		layer1d.NewParamReLUForward(&variable.Param.D1[1], &variable.GetGrad1D()[1]),
 
-		layer1d.NewAffineForward(variable.Param3D[2], variable.Param2D[2], variable.GetGrad3D()[2], variable.GetGrad2D()[2]),
+		layer1d.NewAffineForward(variable.Param.D3[2], variable.Param.D2[2], variable.GetGrad3D()[2], variable.GetGrad2D()[2]),
 		layer1d.NewSigmoidForward(),
 	}
 
@@ -127,13 +150,15 @@ func NewStandardAffine(xn, h1, h2, yn int, c, threshold float64, r *rand.Rand) (
 
 func NewStandardLinearSum(xn int, c, threshold float64) (Sequential, Variable) {
 	variable := Variable{
-		Param1D:tensor.D1{0.0},
-		Param2D:tensor.D2{tensor.NewD1Zeros(xn)},
+		Param:Param{
+			D1:tensor.D1{0.0},
+			D2:tensor.D2{tensor.NewD1Zeros(xn)},
+		},
 	}
 	variable.Init()
 
 	forwards := layer1d.Forwards{
-		layer1d.NewLinearSumForward(variable.Param2D[0], &variable.Param1D[0], variable.GetGrad2D()[0], &variable.GetGrad1D()[0]),
+		layer1d.NewLinearSumForward(variable.Param.D2[0], &variable.Param.D1[0], variable.GetGrad2D()[0], &variable.GetGrad1D()[0]),
 		layer1d.NewSigmoidForward(),
 	}
 
@@ -158,9 +183,9 @@ func (m *Sequential) MeanLoss(x, t tensor.D2) (float64, error) {
 		return 0.0, fmt.Errorf("入力値と正解ラベルのバッチ数が一致しません。")
 	}
 
-	sum := m.Param1DLossFunc(m.variable.Param1D)
-	sum += m.Param2DLossFunc(m.variable.Param2D)
-	sum += m.Param3DLossFunc(m.variable.Param3D)
+	sum := m.Param1DLossFunc(m.variable.Param.D1)
+	sum += m.Param2DLossFunc(m.variable.Param.D2)
+	sum += m.Param3DLossFunc(m.variable.Param.D3)
 	sum *= float64(n)
 
 	for i := range x {
@@ -212,9 +237,9 @@ func (m *Sequential) UpdateGrad(x, t tensor.D1) error {
 		return err
 	}
 
-	grad1D := m.Param1DLossDerivative(m.variable.Param1D)
-	grad2D := m.Param2DLossDerivative(m.variable.Param2D)
-	grad3D := m.Param3DLossDerivative(m.variable.Param3D)
+	grad1D := m.Param1DLossDerivative(m.variable.Param.D1)
+	grad2D := m.Param2DLossDerivative(m.variable.Param.D2)
+	grad3D := m.Param3DLossDerivative(m.variable.Param.D3)
 
 	err = m.variable.grad1D.Add(grad1D)
 	if err != nil {
@@ -268,9 +293,9 @@ func (m *Sequential) ValidateBackwardAndNumericalGradientDifference(x, t tensor.
 		return loss
 	} 
 
-	numGradD1 := mlfuncs1d.NumericalDifferentiation(m.variable.Param1D, lossD1)
-	numGradD2 := mlfuncs2d.NumericalDifferentiation(m.variable.Param2D, lossD2)
-	numGradD3 := mlfuncs3d.NumericalDifferentiation(m.variable.Param3D, lossD3)
+	numGradD1 := mlfuncs1d.NumericalDifferentiation(m.variable.Param.D1, lossD1)
+	numGradD2 := mlfuncs2d.NumericalDifferentiation(m.variable.Param.D2, lossD2)
+	numGradD3 := mlfuncs3d.NumericalDifferentiation(m.variable.Param.D3, lossD3)
 	if m.L2NormGradClipThreshold > 0.0 {
 		mlfuncs.ClipL2Norm(numGradD1, numGradD2, numGradD3, m.L2NormGradClipThreshold)
 	}
