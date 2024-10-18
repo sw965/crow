89bc6c7825cee31a77b5b7dd3802736a09fc3336
sw965 <none>
2024-10-18 21:08:35 +0900

    引数の位置を変更

--- mcts/duct/duct.go
+++ mcts/duct/duct.go
@@ -85,7 +85,7 @@ func (mcts *MCTS[S, Ass, As, A]) SetPlayout(player simultaneous.Player[S, As, A]
 	}
 }
 
-func (mcts *MCTS[S, Ass, As, A]) SetRandPlayout(r *rand.Rand, eval simultaneous.ResultJointEvaluator[S]) {
+func (mcts *MCTS[S, Ass, As, A]) SetRandPlayout(eval simultaneous.ResultJointEvaluator[S], r *rand.Rand) {
 	player := mcts.GameLogic.NewRandActionPlayer(r)
 	mcts.SetPlayout(player, eval)
 }
