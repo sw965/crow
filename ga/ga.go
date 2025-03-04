6560ee49ddc7b1341904d8329c34b1c7c05baf1c
sw965 <none>
2025-03-04 15:58:01 +0900

    遺伝的アルゴリズムの実装

---
+++ ga/ga.go
@@ -0,0 +1,113 @@
+package ga
+
+import (
+	"fmt"
+	"math/rand"
+	"golang.org/x/exp/slices"
+	omwrand "github.com/sw965/omw/math/rand"
+)
+
+type Individual[T any] []T
+
+type Population[T any] []Individual[T]
+
+type Fitness[T any] func(Individual[T]) float64
+
+type Selector[T any] func(Population[T], Fitness[T], *rand.Rand) (Individual[T], error)
+
+func RouletteSelector[T any](p Population[T], fit Fitness[T], r *rand.Rand) (Individual[T], error) {
+	ws := make([]float64, len(p))
+	for i := range ws {
+		y := fit(p[i])
+		if y < 0.0 {
+			return nil, fmt.Errorf("ルーレット選択を用いる場合、適応関数の出力値は0以上でなければならない")
+		}
+		ws[i] = fit(p[i])
+	}
+	idx := omwrand.IntByWeight(ws, r)
+	return p[idx], nil
+}
+
+type CrossOperator[T any] func(Individual[T], Individual[T], *rand.Rand) (Individual[T], Individual[T], error)
+
+func UniformCrossOperator[T any](parent1, parent2 Individual[T], r *rand.Rand) (Individual[T], Individual[T], error) {
+	n := len(parent1)
+	if n != len(parent2) {
+		return nil, nil, fmt.Errorf("個体の長さが一致しない")
+	}
+
+	child1 := make(Individual[T], n)
+	child2 := make(Individual[T], n)
+
+	for i := range parent1 {
+		if r.Float64() < 0.5 {
+			child1[i] = parent1[i]
+			child2[i] = parent2[i]
+		} else {
+			child1[i] = parent2[i]
+			child2[i] = parent1[i]
+		}
+	}
+	return child1, child2, nil
+}
+
+type MutationOperator[T any] func(Individual[T], *rand.Rand) Individual[T]
+
+type Engine[T any] struct {
+	Fitness          Fitness[T]
+	Selector         Selector[T]
+	CrossOperator    CrossOperator[T]
+	MutationOperator MutationOperator[T]
+	CrossPercent     float64
+	MutationPercent  float64
+}
+
+func (e *Engine[T]) Run(init Population[T], generation int, r *rand.Rand) (Population[T], error) {
+	N := len(init)
+	current := init
+
+	for i := 0; i < generation; i++ {
+		next := make(Population[T], 0, N)
+
+		for len(next) < N {
+			t := r.Float64()
+
+			if t < e.CrossPercent {
+				parent1, err := e.Selector(current, e.Fitness, r)
+				if err != nil {
+					return nil, err
+				}
+
+				parent2, err := e.Selector(current, e.Fitness, r)
+				if err != nil {
+					return nil, err
+				}
+				child1, child2, err := e.CrossOperator(parent1, parent2, r)
+				if err != nil {
+					return nil, err
+				}
+
+				next = append(next, child1)
+				if len(next) < N {
+					next = append(next, child2)
+				}
+			} else if t < e.CrossPercent+e.MutationPercent {
+				parent, err := e.Selector(current, e.Fitness, r)
+				if err != nil {
+					return nil, err
+				}
+				mutated := e.MutationOperator(parent, r)
+				next = append(next, mutated)
+			} else {
+				parent, err := e.Selector(current, e.Fitness, r)
+				if err != nil {
+					return nil, err
+				}
+				clone := slices.Clone(parent)
+				next = append(next, clone)
+			}
+		}
+		current = next
+	}
+	return current, nil
+}
---
+++ ga/ga_test.go
@@ -0,0 +1,69 @@
+package ga_test
+
+import (
+	"testing"
+	"github.com/sw965/crow/ga" // 実際の環境に合わせて適切なインポートパスに変更してください
+	"fmt"
+	"math/rand"
+	omwrand "github.com/sw965/omw/math/rand"
+)
+
+func Test(t *testing.T) {
+	r := omwrand.NewMt19937()
+
+	popSize := 20
+	individualLen := 10
+
+	initPop := make(ga.Population[int], popSize)
+	for i := 0; i < popSize; i++ {
+		ind := make(ga.Individual[int], individualLen)
+		for j := 0; j < individualLen; j++ {
+			if r.Float64() < 0.5 {
+				ind[j] = 0
+			} else {
+				ind[j] = 1
+			}
+		}
+		initPop[i] = ind
+	}
+
+	fitness := func(ind ga.Individual[int]) float64 {
+		sum := 0
+		for _, gene := range ind {
+			sum += gene
+		}
+		return float64(sum)
+	}
+
+	selector := ga.RouletteSelector[int]
+	crossOperator := ga.UniformCrossOperator[int]
+
+	mutationOperator := func(ind ga.Individual[int], r *rand.Rand) ga.Individual[int] {
+		mutated := make(ga.Individual[int], len(ind))
+		copy(mutated, ind)
+		pos := r.Intn(len(mutated))
+		if mutated[pos] == 0 {
+			mutated[pos] = 1
+		} else {
+			mutated[pos] = 0
+		}
+		return mutated
+	}
+
+	engine := ga.Engine[int]{
+		Fitness:          fitness,
+		Selector:         selector,
+		CrossOperator:    crossOperator,
+		MutationOperator: mutationOperator,
+		CrossPercent:     0.5,
+		MutationPercent:  0.01,
+	}
+
+	generations := 500
+	finalPop, err := engine.Run(initPop, generations, r)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(finalPop)
+}
